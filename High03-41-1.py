f640e3c16af0fc0014fae5db8b79642b18a5e43e
waka_prings <waka_taka@wakataka.mixh.jp>
2021-02-13 23:28:37 -0500

    sympy Training 20210214-03

--- High03-41-1.py
+++ High03-41-1.py
@@ -18,15 +18,22 @@ tan_theta = (tan_alpha-tan_beta)/(1+tan_alpha*tan_beta)
 # 数式の簡略化
 #print(sym.simplify(tan_theta))
 
-# tan_thetaをグラフ化する
-x_plot = np.arange(0.1,10,0.01)
+# tan_thetaをグラフ化する...(1)
+x_plot = np.arange(0.01,10,0.01)
 y_plot_list = []
 for i in x_plot :
     y_plot_list.append(tan_theta.subs(t, i))
 y_plot = np.array(y_plot_list)
+# (1)をY軸対象にする
+x_plot2 = x_plot*(-1)
+y_plot_list_2 = []
+for i in x_plot2 :
+    y_plot_list_2.append(tan_theta.subs(t, i))
+y_plot2 = np.array(y_plot_list_2)
 
 plt.figure(figsize=(15.0, 6.0))
 plt.plot(x_plot, y_plot, label=str(sym.simplify(tan_theta)), linestyle="-", color='slateblue')
+plt.plot(x_plot2, y_plot2, label=str(sym.simplify(tan_theta))+"Y", linestyle="-", color='slateblue')
 plt.legend()
 plt.savefig('High03-41-1_01.png')
 plt.close()
